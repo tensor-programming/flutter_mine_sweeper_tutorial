550e6f2f1f7e200127254bcb9bc3c200c4ab8b00
Tensor-Programming <abeltensor@tensor-programming.com>
2018-04-07 19:36:15 -0400

    Part 3 initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 enum TileState { covered, blown, open, flagged, revealed }
 
@@ -25,11 +26,28 @@ class BoardState extends State<Board> {
   final int numOfMines = 11;
 
   List<List<TileState>> uiState;
+  List<List<bool>> tiles;
 
   void resetBoard() {
     uiState = new List<List<TileState>>.generate(rows, (row) {
       return new List<TileState>.filled(cols, TileState.covered);
     });
+
+    tiles = new List<List<bool>>.generate(rows, (row) {
+      return new List<bool>.filled(cols, false);
+    });
+
+    Random random = Random();
+    int remainingMines = numOfMines;
+    while (remainingMines > 0) {
+      int pos = random.nextInt(rows * cols);
+      int row = pos ~/ rows;
+      int col = pos % cols;
+      if (!tiles[row][col]) {
+        tiles[row][col] = true;
+        remainingMines--;
+      }
+    }
   }
 
   @override
@@ -40,27 +58,37 @@ class BoardState extends State<Board> {
 
   Widget buildBoard() {
     List<Row> boardRow = <Row>[];
-    for (int i = 0; i < rows; i++) {
+    for (int y = 0; y < rows; y++) {
       List<Widget> rowChildren = <Widget>[];
-      for (int j = 0; j < cols; j++) {
-        TileState state = uiState[i][j];
-        if (state == TileState.covered) {
+      for (int x = 0; x < cols; x++) {
+        TileState state = uiState[y][x];
+        int count = mineCount(x, y);
+        if (state == TileState.covered || state == TileState.flagged) {
           rowChildren.add(GestureDetector(
+            onLongPress: () {
+              flag(x, y);
+            },
+            onTap: () {
+              probe(x, y);
+            },
             child: Listener(
-              child: Container(
-                margin: EdgeInsets.all(1.0),
-                height: 20.0,
-                width: 20.0,
-                color: Colors.grey,
-              ),
-            ),
+                child: CoveredMineTile(
+              flagged: state == TileState.flagged,
+              posX: x,
+              posY: y,
+            )),
+          ));
+        } else {
+          rowChildren.add(OpenMineTile(
+            state: state,
+            count: count,
           ));
         }
       }
       boardRow.add(Row(
         children: rowChildren,
         mainAxisAlignment: MainAxisAlignment.center,
-        key: ValueKey<int>(i),
+        key: ValueKey<int>(y),
       ));
     }
     return Container(
@@ -85,5 +113,164 @@ class BoardState extends State<Board> {
         ),
       ),
     );
+  }
+
+  void probe(int x, int y) {
+    if (uiState[y][x] == TileState.flagged) return;
+    setState(() {
+      if (tiles[y][x]) {
+        uiState[y][x] = TileState.blown;
+      } else {
+        open(x, y);
+      }
+    });
+  }
+
+  void open(int x, int y) {
+    if (!inBoard(x, y)) return;
+    if (uiState[y][x] == TileState.open) return;
+    uiState[y][x] = TileState.open;
+
+    if (mineCount(x, y) > 0) return;
+
+    open(x - 1, y);
+    open(x + 1, y);
+    open(x, y - 1);
+    open(x, y + 1);
+    open(x - 1, y - 1);
+    open(x + 1, y + 1);
+    open(x + 1, y - 1);
+    open(x - 1, y + 1);
+  }
+
+  void flag(int x, int y) {
+    setState(() {
+      if (uiState[y][x] == TileState.flagged) {
+        uiState[y][x] = TileState.covered;
+      } else {
+        uiState[y][x] = TileState.flagged;
+      }
+    });
+  }
+
+  int mineCount(int x, int y) {
+    int count = 0;
+    count += bombs(x - 1, y);
+    count += bombs(x + 1, y);
+    count += bombs(x, y - 1);
+    count += bombs(x, y + 1);
+    count += bombs(x - 1, y - 1);
+    count += bombs(x + 1, y + 1);
+    count += bombs(x + 1, y - 1);
+    count += bombs(x - 1, y + 1);
+    return count;
+  }
+
+  int bombs(int x, int y) => inBoard(x, y) && tiles[y][x] ? 1 : 0;
+  bool inBoard(int x, int y) => x >= 0 && x < cols && y >= 0 && y < rows;
+}
+
+Widget buildTile(Widget child) {
+  return Container(
+    padding: EdgeInsets.all(1.0),
+    height: 30.0,
+    width: 30.0,
+    color: Colors.grey[400],
+    margin: EdgeInsets.all(2.0),
+    child: child,
+  );
+}
+
+Widget buildInnerTile(Widget child) {
+  return Container(
+    padding: EdgeInsets.all(1.0),
+    margin: EdgeInsets.all(2.0),
+    height: 20.0,
+    width: 20.0,
+    child: child,
+  );
+}
+
+class CoveredMineTile extends StatelessWidget {
+  final bool flagged;
+  final int posX;
+  final int posY;
+
+  CoveredMineTile({this.flagged, this.posX, this.posY});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget text;
+    if (flagged) {
+      text = buildInnerTile(RichText(
+        text: TextSpan(
+          text: "\u2691",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        textAlign: TextAlign.center,
+      ));
+    }
+    Widget innerTile = Container(
+      padding: EdgeInsets.all(1.0),
+      margin: EdgeInsets.all(2.0),
+      height: 20.0,
+      width: 20.0,
+      color: Colors.grey[350],
+      child: text,
+    );
+
+    return buildTile(innerTile);
+  }
+}
+
+class OpenMineTile extends StatelessWidget {
+  final TileState state;
+  final int count;
+  OpenMineTile({this.state, this.count});
+
+  final List textColor = [
+    Colors.blue,
+    Colors.green,
+    Colors.red,
+    Colors.purple,
+    Colors.cyan,
+    Colors.amber,
+    Colors.brown,
+    Colors.black,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    Widget text;
+
+    if (state == TileState.open) {
+      if (count != 0) {
+        text = RichText(
+          text: TextSpan(
+            text: '$count',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: textColor[count - 1],
+            ),
+          ),
+          textAlign: TextAlign.center,
+        );
+      }
+    } else {
+      text = RichText(
+        text: TextSpan(
+          text: '\u2739',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+          ),
+        ),
+        textAlign: TextAlign.center,
+      );
+    }
+    return buildTile(buildInnerTile(text));
   }
 }
